d5c3e51d4bb789282620045be16308aee277bb91
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-11-29 21:05:31 +0800

    display seller's product in product page

--- seller/sellerProduct.php
+++ seller/sellerProduct.php
@@ -1,5 +1,7 @@
 <?php
 include_once('../config/bootstrap.php');
+require_once('../config/connect_db.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,6 +23,59 @@ include_once('../config/bootstrap.php');
                 <div class="row">
                     <div class="col-md-12 mt-2">
                         <a href="sellerAddProduct.php" class="btn btn-sm btn btn-outline-dark btn-block" style="border-radius: 0px ;">Add Product</a>
+                    </div>
+
+                    <div class="col-md-12 mt-2">
+                        <div class="card border-dark">
+                            <div class="card-header border-dark bg-dark text-white">
+                                Lists of Products
+                            </div>
+                            
+                            <div class="row">
+                                <?php
+                                    $sql = "SELECT * FROM product WHERE prdt_seller = '".$_SESSION["user_id"]."'";
+                                    $result = mysqli_query($conn, $sql);
+                                    if(mysqli_num_rows($result) > 0){
+                                        while($row = mysqli_fetch_assoc($result)){
+                                ?>
+                                    <div class="col-md-3 mx-2 my-2">
+                                        <div class="card border-dark">
+                                            <img src="<?php echo $row['prdt_image']; ?>" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">
+                                                    <?php echo $row['prdt_name']; ?>
+                                                </h5>
+                                            
+                                                <div class="card-subtitle mb-0 text-muted">
+                                                    <?php 
+                                                        if($row['prdt_available']>0){
+                                                            echo '<p class="text-success">Available</p>';
+                                                        }else{
+                                                            echo '<p class="text-danger">Unvailable</p>';
+                                                        }
+                                                    ?>
+                                                </div>
+
+                                                <div class="card-text">
+                                                    Price: <?php echo $row['prdt_sellPrice']; ?> <br>
+                                                    Type: <?php echo $row['prdt_type'] ?> <br>
+                                                    Quantity left: <?php echo $row['prdt_quantity']; ?> <br>
+                                                    Description: <?php echo $row['prdt_desc']; ?>
+                                                </div>
+
+                                                <div class="text-right">
+                                                    <button href="#" class="btn btn-sm btn-outline-warning">Edit</button>
+                                                    <button href="#" class="btn btn-sm btn-outline-danger">Delete</button>
+                                                </div>
+                                            </div>    
+                                        </div>
+                                    </div>
+                                <?php
+                                        }
+                                    }
+                                ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
