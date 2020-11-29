106796d12fcfda100c87bb1f01ebbe226b704f61
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-11-29 17:15:44 +0800

    top & side navs for seller page

---
+++ seller/layouts/sellerSideNav.php
@@ -0,0 +1,37 @@
+<nav class="k-side-nav nav flex-column col-2 border-right bg-white p-0">
+    <div class="logo">
+        <h2>
+            <a href="#" class="d-flex d-md-none"><i class="fab fa-apple"></i></a>
+            <a href="#" class="d-none d-md-flex"><i class="fab fa-apple  mr-1"></i>APPLE</a>
+        </h2>
+    </div>
+    <div class="k-nav-container h-75">
+        <ul class="k-nav nav">
+            <li class="nav-item w-100 mb-1">
+                <a href="sellerHome.php" class="nav-link w-100 <?php if($pageName == 'Home') echo 'active';?>">
+                <i class="fas fa-home"></i>
+                    <span class="d-none d-lg-inline-flex ml-3">Home</span>
+                </a>
+            </li>
+            <li class="nav-item w-100 mb-1">
+                <a href="sellerProduct.php" class="nav-link w-100 <?php if($pageName == 'Product') echo 'active';?>">
+                    <i class="fas fa-bars d-inline-flex"></i>
+                    <span class="d-none d-lg-inline-flex ml-3">Products</span>
+                </a>
+            </li>
+            <li class="nav-item w-100 mb-1">
+                <a href="sellerChat.php" class="nav-link w-100 <?php if($pageName == 'Chat') echo 'active';?>">
+                    <i class="fas fa-comments"></i>
+                    <span class="d-none d-lg-inline-flex ml-3">Chats</span>
+                </a>
+            </li>
+            <li class="nav-item w-100 mb-1">
+                <a href="sellerPromo.php" class="nav-link w-100 <?php if($pageName == 'Promo') echo 'active';?>">
+                    <i class="fas fa-ad"></i>
+                    <span class="d-none d-lg-inline-flex ml-3">Promo</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
---
+++ seller/sellerChat.php
@@ -0,0 +1,18 @@
+<?php
+include_once('../config/bootstrap.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <?php echo $bootstrapCSS; echo $jQueryJS; echo $bootstrapJS; echo $fontAwsomeIcons ?>
+</head>
+    <link rel="stylesheet" href="layouts/navBar.css"/>
+<body>
+    <?php
+        $pageName = 'Chat';
+        include 'layouts/sellerSideNav.php';
+        include 'layouts/sellerTopNav.php';
+    ?>
+</body>
+</html>
