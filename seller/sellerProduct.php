b45ea7e2c2796da893a9154b890db26aa4de4e14
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-12-06 13:35:51 +0800

    add missing meta view port & adjust for super small phone device

--- seller/sellerProduct.php
+++ seller/sellerProduct.php
@@ -7,6 +7,8 @@ include_once('../functions/checkSession.php');
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title></title>
     <?php echo $bootstrapCSS; echo $jQueryJS; echo $bootstrapJS; echo $fontAwsomeIcons ?>
 </head>
