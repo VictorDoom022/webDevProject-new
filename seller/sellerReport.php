b340e3d2ca46c2a5b2d1b0cd0b7a15068c302231
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-12-29 01:40:35 +0800

    export table to csv function

--- seller/sellerReport.php
+++ seller/sellerReport.php
@@ -117,9 +117,61 @@ include_once('../functions/checkSession.php');
                             </table>
                         </div>
                     </div>
+                    <div class="col-md-12 mt-2">
+                        <input type="button" id="gnerateCSV" class="btn btn-sm btn btn-outline-dark btn-block" style="border-radius: 0px ;" value="Generate CSV"/>
+                    </div>
                 </div>
             </main>
         </div>
     </div>
 </body>
+<script>
+    function download_csv(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+}
+
+function export_table_to_csv(html, filename) {
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+	
+    for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+		csv.push(row.join(","));		
+	}
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+}
+
+document.querySelector("#gnerateCSV").addEventListener("click", function () {
+    var html = document.querySelector("table").outerHTML;
+	export_table_to_csv(html, "table.csv");
+});
+</script>
 </html>
