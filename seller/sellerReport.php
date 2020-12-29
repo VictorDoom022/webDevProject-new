0e9c2384ec67bbc749b377a669315c8638ad95a0
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-12-29 12:59:27 +0800

    move generate CSV JS into /functions & show more data in CSV file

--- seller/sellerReport.php
+++ seller/sellerReport.php
@@ -56,6 +56,21 @@ include_once('../functions/checkSession.php');
                                 <h4 class="text-center">RM<?php echo $row['totalUnitPrice'] ?></h4>
                         </div>
                     </div>
+
+                    <table style="display:none">
+                        <tr>
+                            <td>Total Revenue(RM) :</td>
+                            <td><?php echo $row['totalSellPrice'] - $row['totalOriPrice']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Total Product Sold in Original Price(RM) :</td>
+                            <td><?php echo $row['totalOriPrice']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Total Product Sold in Selling Price(RM) :</td>
+                            <td><?php echo $row['totalUnitPrice'] ?></td>
+                        </tr>
+                    </table>
                 <?php
                         }
                     }
@@ -125,53 +140,5 @@ include_once('../functions/checkSession.php');
         </div>
     </div>
 </body>
-<script>
-    function download_csv(csv, filename) {
-    var csvFile;
-    var downloadLink;
-
-    // CSV FILE
-    csvFile = new Blob([csv], {type: "text/csv"});
-
-    // Download link
-    downloadLink = document.createElement("a");
-
-    // File name
-    downloadLink.download = filename;
-
-    // We have to create a link to the file
-    downloadLink.href = window.URL.createObjectURL(csvFile);
-
-    // Make sure that the link is not displayed
-    downloadLink.style.display = "none";
-
-    // Add the link to your DOM
-    document.body.appendChild(downloadLink);
-
-    // Lanzamos
-    downloadLink.click();
-}
-
-function export_table_to_csv(html, filename) {
-	var csv = [];
-	var rows = document.querySelectorAll("table tr");
-	
-    for (var i = 0; i < rows.length; i++) {
-		var row = [], cols = rows[i].querySelectorAll("td, th");
-		
-        for (var j = 0; j < cols.length; j++) 
-            row.push(cols[j].innerText);
-        
-		csv.push(row.join(","));		
-	}
-
-    // Download CSV
-    download_csv(csv.join("\n"), filename);
-}
-
-document.querySelector("#gnerateCSV").addEventListener("click", function () {
-    var html = document.querySelector("table").outerHTML;
-	export_table_to_csv(html, "table.csv");
-});
-</script>
+<script src="../functions/seller/generateCSV.js"></script>
 </html>
