f420eb77b47b6f51f0871780f592b6454da312ee
victordoom022 <victordoom022@gmail.com>
2021-01-01 23:08:45 +0800

    show all users in chat

--- seller/sellerChat.php
+++ seller/sellerChat.php
@@ -1,5 +1,6 @@
 <?php
 include_once('../config/bootstrap.php');
+require_once('../config/connect_db.php');
 session_start();
 include_once('../functions/checkSession.php');
 ?>
@@ -12,11 +13,54 @@ include_once('../functions/checkSession.php');
     <?php echo $bootstrapCSS; echo $jQueryJS; echo $bootstrapJS; echo $fontAwsomeIcons ?>
 </head>
     <link rel="stylesheet" href="layouts/navBar.css"/>
+    <link rel="stylesheet" href="layouts/chats.css"/>
 <body>
     <?php
         $pageName = $pageTitle = 'Chat';
         include 'layouts/sellerSideNav.php';
         include 'layouts/sellerTopNav.php';
     ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <main class="col-md-10">
+                <div class="row">
+                    <div class="col-md-12 px-0">
+                     
+                        <div class="inbox_msg">
+                            <div class="inbox_people">
+                                <div class="inbox_chat">
+                                    <?php
+                                        $sql = "SELECT * FROM users";
+                                        $result = mysqli_query($conn, $sql);
+                                        if(mysqli_num_rows($result) > 0){
+                                            while($row = mysqli_fetch_assoc($result)){
+                                    ?>
+                                        <div class="chat_list">
+                                            <div class="chat_people">
+                                                <div class="chat_img"> 
+                                                    <img src="https://ptetutorials.com/images/user-profile.png" > 
+                                                </div>
+                                                <div class="chat_ib">
+                                                    <h5>
+                                                        <?php echo $row['username'] ?>
+                                                        <span class="chat_date">Dec 25</span>
+                                                    </h5>
+                                                    <p>msg</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
