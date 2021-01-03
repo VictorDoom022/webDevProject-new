2d06faf0b130a7d1bae3b27c46767b5f220cd9c0
victordoom022 <victordoom022@gmail.com>
2021-01-03 15:00:30 +0800

    set script from bootstrap.php & chat css update from seller

--- admin/adminChat.php
+++ admin/adminChat.php
@@ -10,12 +10,7 @@ include_once('../functions/checkSession.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <!-- jQuery and JS bundle w/ Popper.js -->
-    <?php echo $jQueryJS;?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <?php echo $bootstrapCSS; echo $jQueryJS; echo $jQueryFormJS; echo $bootstrapJS; echo $fontAwsomeIcons ?>
 </head>
 <link rel="stylesheet" href="layouts/navBar.css"/>
 <link rel="stylesheet" href="layouts/chats.css"/>
@@ -25,7 +20,7 @@ include_once('../config/bootstrap.php');
 include_once('../config/connect_db.php');
 ?>
     <?php
-        $pageName = $pageTitle = 'Admin';
+        $pageName = $pageTitle = 'Chat';
         include 'layouts/adminSideNav.php';
         include 'layouts/adminTopNav.php';
     ?>
--- admin/layouts/adminTopNav.php
+++ admin/layouts/adminTopNav.php
@@ -1,8 +1,10 @@
 <nav class="k-top-nav navbar navbar-expand-lg navbar-light bg-white col-10 pl-4 border-bottom">
 	<span class="navbar-brand h1 mb-0 col">
-        <?php if ($pageName == 'Admin'):?>
+        <?php if ($pageName == 'Admin'){?>
             <i class="fas fa-home"></i>
-        <?php endif; ?>
+        <?php }else if ($pageName == 'Chat'){?>
+			<i class="fas fa-comments"></i>
+		<?php } ?>
         <span class="d-none d-lg-inline-flex ml-3"><?php echo $pageTitle; ?></span>
         
     </span>
