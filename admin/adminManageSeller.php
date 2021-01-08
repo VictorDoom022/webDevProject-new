fabb4f1c39a400b5d2ede98f1130ec29920ad397
tuanhee <teotuanhee1yee2@gmail.com>
2021-01-09 00:01:56 +0800

    fix icon problem

--- admin/adminManageSeller.php
+++ admin/adminManageSeller.php
@@ -1,3 +1,7 @@
+<?php
+include_once('../config/bootstrap.php');
+include_once('../config/connect_db.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,15 +12,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <!-- jQuery and JS bundle w/ Popper.js -->
+    <?= $fontAwsomeIcons ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 <link rel="stylesheet" href="layouts/navBar.css"/>
 <body>
-<?php
-include_once('../config/bootstrap.php');
-include_once('../config/connect_db.php');
-?>
     <?php
         $pageName = $pageTitle = 'Admin';
         include 'layouts/adminSideNav.php';
