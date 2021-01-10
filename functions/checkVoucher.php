114fc7deb6576dd07c28a513f8c9d0fa1cff6eab
tuanhee <teotuanhee1yee2@gmail.com>
2021-01-10 16:19:50 +0800

    change discount from amount to discount

--- functions/checkVoucher.php
+++ functions/checkVoucher.php
@@ -10,15 +10,19 @@ $_SERVER['REQUEST_METHOD'] == "GET"){
 
     $promo_code = $_GET['promo_code'];
 
-    $sql = "SELECT * FROM promo WHERE promo_code = '$promo_code'";
+    $sql = "SELECT * FROM promo LEFT JOIN product ON promo.promo_prdt = product.id WHERE promo_code = '$promo_code'";
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result) > 0){
         $data['voucherExists'] = "1";
         while($row = mysqli_fetch_assoc($result)){
+            $product_price = $row['prdt_sellPrice'];
+
+            $data['promo_prdt'] = $row['promo_prdt'];
+
             $data['promo_code'] = $row['promo_code'];
             $data['promo_prdt'] = $row['promo_prdt'];
-            $data['promo_discount'] = $row['promo_discount'];
+            $data['promo_discount'] = $product_price * $row['promo_discount'] / 100;
         }
     }else{
         $data['voucherExists'] = "0";
