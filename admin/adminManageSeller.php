7ab54459d59b5dc44a3d8bfd14fb0ab9586c4ddc
tuanhee <teotuanhee1yee2@gmail.com>
2021-01-10 23:42:09 +0800

    fix bugs

--- admin/adminManageSeller.php
+++ admin/adminManageSeller.php
@@ -25,39 +25,41 @@ include_once('../functions/checkSession.php');
         include 'layouts/adminSideNav.php';
         include 'layouts/adminTopNav.php';
     ?>
-<main>
-<div class="container">
+<div class="container-fluid">
     <?php 
         $query = "SELECT * FROM users WHERE position = 'seller'";
         $result = mysqli_query($conn,$query);
         if(!$result) die ('Get data failed');
     ?>
     <div class="row">
-        <table class="table border">
-            <thead>
-                <tr>
-                    <th>Seller Name</th>
-                    <th>Password</th>
-                    <th>Email</th>
-                    <th>Position</th>
-                </tr>
-            </thead>
-            <?php
-                while($row = $result->fetch_assoc()):
-            ?>
-                <tr>
-                    <td><?php echo $row['username']?></td>
-                    <td><?php echo $row['password']?></td>
-                    <td><?php echo $row['email']?></td>
-                    <td><?php echo $row['position']?></td>
-                </tr>
-        <?php endwhile;?>
-        </table>
-        <div class="col-md-12 text-center">
-            <a href="adminSelectSeller.php" class="btn btn-outline-warning col-md-3">Edit</a>
-            <a href="adminDeleteSeller.php" class="btn btn-outline-danger col-md-3">Delete</a>
-            <a href="adminAddSeller.php" class="btn btn-outline-success col-md-3">Add</a>
-        </div>
+        <div class="col-md-2"></div>
+        <main class="col-md-10 pt-3">
+            <table class="table border">
+                <thead>
+                    <tr>
+                        <th>Seller Name</th>
+                        <th>Password</th>
+                        <th>Email</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+                <?php
+                    while($row = $result->fetch_assoc()):
+                ?>
+                    <tr>
+                        <td><?php echo $row['username']?></td>
+                        <td><?php echo $row['password']?></td>
+                        <td><?php echo $row['email']?></td>
+                        <td><?php echo $row['position']?></td>
+                    </tr>
+            <?php endwhile;?>
+            </table>
+            <div class="text-center">
+                <a href="adminSelectSeller.php" class="btn btn-outline-warning col-md-3">Edit</a>
+                <a href="adminDeleteSeller.php" class="btn btn-outline-danger col-md-3">Delete</a>
+                <a href="adminAddSeller.php" class="btn btn-outline-success col-md-3">Add</a>
+            </div>
+        </main>
     </div><br>
 </div>
 </main>
