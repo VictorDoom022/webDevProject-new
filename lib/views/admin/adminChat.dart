2ce9a66cf8d92b48e64b388ae26e1120ba5fbc8f
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-02-18 22:50:00 +0800

    display all user in chat (admin & seller)

--- lib/views/admin/adminChat.dart
+++ lib/views/admin/adminChat.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:myapp/controllers/admin/fetchUsers.dart';
+import 'package:myapp/model/userClass.dart';
 import 'package:myapp/views/admin/navDrawerAdmin.dart';
 
 class adminChat extends StatefulWidget {
@@ -23,13 +26,94 @@ class adminChatList extends StatefulWidget {
 }
 
 class _adminChatListState extends State<adminChatList> {
+
+  Future<List<Users>> futureUsers;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureUsers = getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Chat Lists"),
       ),
+      body: FutureBuilder<List<Users>>(
+        future: futureUsers,
+        builder: (context, snapshot){
+          if(snapshot.hasError) print(snapshot.error);
+
+          if(snapshot.hasData){
+            return adminUserChatList(users: snapshot.data);
+          }else{
+            return Center(child: SpinKitCircle(color: Colors.cyan, size: 70.0,));
+          }
+        },
+      ),
       drawer: navDrawerAdmin(),
     );
   }
 }
+
+class adminUserChatList extends StatelessWidget {
+
+  final List<Users> users;
+
+  adminUserChatList({Key key, this.users}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: InkWell(
+              onTap: (){
+                print('Tap');
+              },
+              child: Container(
+                height: 80,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              users[index].username,
+                              style: TextStyle(
+                                fontSize: 28,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              users[index].position,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w300
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+    );
+  }
+}
+
--- lib/views/seller/sellerChat.dart
+++ lib/views/seller/sellerChat.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:myapp/controllers/admin/fetchUsers.dart';
+import 'package:myapp/model/userClass.dart';
 
 import 'navDrawerSeller.dart';
 
@@ -27,14 +30,94 @@ class sellerChatList extends StatefulWidget {
 }
 
 class _sellerChatListState extends State<sellerChatList> {
+
+  Future<List<Users>> futureUsers;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureUsers = getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Chat Lists"),
       ),
+      body: FutureBuilder<List<Users>>(
+        future: futureUsers,
+        builder: (context, snapshot){
+          if(snapshot.hasError) print(snapshot.error);
+
+          if(snapshot.hasData){
+            return sellerUserChatList(users: snapshot.data);
+          }else{
+            return Center(child: SpinKitCircle(color: Colors.cyan, size: 70.0,));
+          }
+        },
+      ),
       drawer: navDrawerSeller(),
     );
   }
 }
+
+class sellerUserChatList extends StatelessWidget {
+  final List<Users> users;
+
+  sellerUserChatList({Key key, this.users}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index){
+            return Card(
+              child: InkWell(
+                onTap: (){
+                  print('Tap');
+                },
+                child: Container(
+                  height: 80,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                  users[index].username,
+                                style: TextStyle(
+                                  fontSize: 28,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                users[index].position,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w300
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+        }
+    );
+  }
+}
+
 
